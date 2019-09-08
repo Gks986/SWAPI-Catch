67b7f4d7d18a3491d281a70edd1edede03996ec8
Rafael Tannous <54330194+Gks986@users.noreply.github.com>
2019-09-08 01:48:48 -0300

    Now with Loading icon and also more data

--- ConsumindoSwapi/app.js
+++ ConsumindoSwapi/app.js
@@ -2,6 +2,7 @@ let button =document.querySelector('#button');
 let name =document.querySelector('#nome');
 let  AnoDenascimento =document.querySelector('#AnoNasc');
 let  massa =document.querySelector('#mass');
+let  altura =document.querySelector('#height');
 function pegaInfo() {
     upgradeInfoComCarregamento()
     let ApiUrl= 'https://swapi.co/api/people/1';
@@ -13,10 +14,11 @@ function upgradeInfo(data){
  name.innerText = `Nome: ${data.name}`;
  AnoDenascimento.innerText =`Data Nascimento: ${ data.birth_year}`;
  massa.innerText = `Peso: ${data.mass} Kg`;
+ altura.innerText = `Altura: ${data.height} cm`;
 }
 function upgradeInfoComCarregamento(){
-    name.innerText ='Loading...';
-    AnoDenascimento.innerText ='';
-    massa.innerText = '';
+    name.innerHTML='<i class="fa fa-circle-o-notch fa-spin  fa-fw"></i>'
+    AnoDenascimento.innerText =''
+    massa.innerText = ''
    }
 button.addEventListener('click',pegaInfo);
